dab9e51c35545b369d4edd437778b1b63dbb381b
Krushi24112002 <krushimonpara24@gmail.com>
2024-08-30 13:50:01 +0530

    JOIN OPERATIONS

--- JOIN_OPERATIONS/JOIN OPERATIONS.sql
+++ JOIN_OPERATIONS/JOIN OPERATIONS.sql
@@ -1,45 +1,59 @@
 CREATE DATABASE JOIN_TEST;
 
+DROP DATABASE JOIN_TEST;
+
 SHOW DATABASES;
 
 USE JOIN_TEST;
 
-CREATE TABLE
-    PROJECT (
-        ID INT,
-        EMP_ID INT,
-        PROJECT_NAME VARCHAR(255),
-        PROJECT_STARTDATE DATE,
-        CLIENT_ID INT
-    );
+CREATE TABLE PROJECT (
+    PROJECT_ID INT,
+    EMP_ID INT,
+    PROJECT_NAME VARCHAR(255),
+    PROJECT_START_DATE DATE,
+    CLIENT_ID INT
+);
 
 SHOW TABLES;
 
-INSERT INTO PROJECT
-VALUES (
-        ID,
+INSERT INTO
+    PROJECT (
+        PROJECT_ID,
         EMP_ID,
         PROJECT_NAME,
-        PROJECT_STARTDATE,
+        PROJECT_START_DATE,
         CLIENT_ID
-    ), (1, 1, 'A', '2021-04-21', 3), (2, 2, 'B', '2021-03-12', 1), (3, 3, 'C', '2021-01-16', 5), (4, 3, 'D', '2021-04-27', 2), (5, 5, 'E', '2021-05-01', 4);
+    )
+VALUES (1, 1, 'A', '2021-04-21', 3),
+    (2, 2, 'B', '2021-03-12', 1),
+    (3, 3, 'C', '2021-01-16', 5),
+    (4, 3, 'D', '2021-04-27', 2),
+    (5, 5, 'E', '2021-05-01', 4);
 
 SELECT * FROM PROJECT;
 
-CREATE TABLE
-    EMPLOYEE (
-        ID INT,
-        FIRST_NAME VARCHAR(255),
-        LAST_NAME VARCHAR(255),
-        AGE INT,
-        EMAIL_ID VARCHAR(255),
-        PHONE_NO INT,
-        CITY VARCHAR(255)
-    );
+CREATE TABLE EMPLOYEE (
+    EMP_ID INT,
+    FIRST_NAME VARCHAR(255),
+    LAST_NAME VARCHAR(255),
+    AGE INT,
+    EMAIL_ID VARCHAR(255),
+    PHONE_NO INT,
+    CITY VARCHAR(255)
+);
 
 SHOW TABLES;
 
-INSERT INTO EMPLOYEE
+INSERT INTO
+    EMPLOYEE (
+        EMP_ID,
+        FIRST_NAME,
+        LAST_NAME,
+        AGE,
+        EMAIL_ID,
+        PHONE_NO,
+        CITY
+    )
 VALUES (
         1,
         'Aman',
@@ -48,7 +62,8 @@ VALUES (
         'aman@gmail.com',
         898,
         'Delhi'
-    ), (
+    ),
+    (
         2,
         'Yagya',
         'Narayan',
@@ -56,7 +71,8 @@ VALUES (
         'yagya@gmail.com',
         222,
         'Palam'
-    ), (
+    ),
+    (
         3,
         'Rahul',
         'BD',
@@ -64,7 +80,8 @@ VALUES (
         'rahul@gmail.com',
         444,
         'Kolkata'
-    ), (
+    ),
+    (
         4,
         'Jatin',
         'Herti',
@@ -72,7 +89,8 @@ VALUES (
         'jatin@gmail.com',
         666,
         'Raipur'
-    ), (
+    ),
+    (
         5,
         'PK',
         'Pandey',
@@ -84,21 +102,30 @@ VALUES (
 
 SELECT * FROM EMPLOYEE;
 
-CREATE TABLE
-    CLIENTS (
-        ID INT,
-        FIRST_NAME VARCHAR(255),
-        LAST_NAME VARCHAR(255),
-        AGE INT,
-        EMAIL_ID VARCHAR(255),
-        PHONE_NO INT,
-        CITY VARCHAR(255),
-        EMP_ID INT
-    );
+CREATE TABLE CLIENTS (
+    CLIENTS_ID INT,
+    FIRST_NAME VARCHAR(255),
+    LAST_NAME VARCHAR(255),
+    AGE INT,
+    EMAIL_ID VARCHAR(255),
+    PHONE_NO INT,
+    CITY VARCHAR(255),
+    EMP_ID INT
+);
 
 SHOW TABLES;
 
-INSERT INTO CLIENTS
+INSERT INTO
+    CLIENTS (
+        CLIENTS_ID,
+        FIRST_NAME,
+        LAST_NAME,
+        AGE,
+        EMAIL_ID,
+        PHONE_NO,
+        CITY,
+        EMP_ID
+    )
 VALUES (
         1,
         'Mac',
@@ -108,7 +135,8 @@ VALUES (
         333,
         'Kolkata',
         3
-    ), (
+    ),
+    (
         2,
         'Max',
         'Poirier',
@@ -117,7 +145,8 @@ VALUES (
         222,
         'Kolkata',
         3
-    ), (
+    ),
+    (
         3,
         'Peter',
         'Jain',
@@ -126,16 +155,18 @@ VALUES (
         111,
         'Delhi',
         1
-    ), (
+    ),
+    (
         4,
         'Sushant',
         'Aggarwal',
         23,
         'sushant@yahoo.com',
         45454,
-        'Hydrabad',
+        'Hyderabad',
         5
-    ), (
+    ),
+    (
         5,
         'Pratap',
         'Singh',
@@ -148,36 +179,28 @@ VALUES (
 
 SELECT * FROM CLIENTS;
 
+SHOW TABLES;
+
 -- INNER JOIN [QUESTION :- 1]
 
--- Enlist All The Employees ID's, Names Along With The Projec Allocated To Them.
+-- Enlist All The Employees ID's, Names Along With The Project Allocated To Them.
 
-SELECT
-    EMPLOYEE.ID,
-    EMPLOYEE.FIRST_NAME,
-    EMPLOYEE.LAST_NAME,
-    PROJECT.ID,
-    PROJECT.PROJECT_NAME
+SELECT EMPLOYEE.EMP_ID, EMPLOYEE.FIRST_NAME, EMPLOYEE.LAST_NAME, PROJECT.PROJECT_ID, PROJECT.PROJECT_NAME
 FROM EMPLOYEE
-    INNER JOIN PROJECT ON EMPLOYEE.ID = PROJECT.EMP_ID;
+    INNER JOIN PROJECT ON EMPLOYEE.EMP_ID = PROJECT.EMP_ID;
 
 -- OR
 
 SELECT *
 FROM EMPLOYEE
-    INNER JOIN PROJECT ON EMPLOYEE.ID = PROJECT.ID;
+    INNER JOIN PROJECT ON EMPLOYEE.EMP_ID = PROJECT.EMP_ID;
 
 -- #NOTE# Without Use Of INNER JOIN Keyword Perform Same Operations.
 
-SELECT
-    EMPLOYEE.ID,
-    EMPLOYEE.FIRST_NAME,
-    EMPLOYEE.LAST_NAME,
-    PROJECT.ID,
-    PROJECT.PROJECT_NAME
+SELECT EMPLOYEE.EMP_ID, EMPLOYEE.FIRST_NAME, EMPLOYEE.LAST_NAME, PROJECT.PROJECT_ID, PROJECT.PROJECT_NAME
 FROM EMPLOYEE, PROJECT
 WHERE
-    EMPLOYEE.ID = PROJECT.EMP_ID;
+    EMPLOYEE.EMP_ID = PROJECT.EMP_ID;
 
 -- INNER JOIN [QUESTION :- 2]
 
@@ -185,17 +208,12 @@ WHERE
 
 -- from Jaipur with the clients name working in Hyderabad.
 
-SELECT
-    EMPLOYEE.ID,
-    EMPLOYEE.EMAIL_ID,
-    EMPLOYEE.PHONE_NO,
-    CLIENTS.FIRST_NAME,
-    CLIENTS.LAST_NAME
+SELECT EMPLOYEE.EMP_ID, EMPLOYEE.EMAIL_ID, EMPLOYEE.PHONE_NO, CLIENTS.FIRST_NAME, CLIENTS.LAST_NAME
 FROM EMPLOYEE
-    INNER JOIN CLIENTS ON EMPLOYEE.ID = CLIENTS.EMP_ID
+    INNER JOIN CLIENTS ON EMPLOYEE.EMP_ID = CLIENTS.EMP_ID
 WHERE
     EMPLOYEE.CITY = 'jaipur'
-    AND CLIENTS.CITY = 'Hydrabad';
+    AND CLIENTS.CITY = 'Hyderabad';
 
 -- LEFT JOIN [QUESTION :- 1]
 
@@ -203,24 +221,19 @@ WHERE
 
 SELECT *
 FROM EMPLOYEE
-    LEFT JOIN PROJECT ON EMPLOYEE.ID = PROJECT.EMP_ID;
+    LEFT JOIN PROJECT ON EMPLOYEE.EMP_ID = PROJECT.EMP_ID;
 
 -- RIGHT JOIN [QUESTION :- 1]
 
--- List out att the projects along with the employee's name and their respective attocated email ID.
+-- List out all the projects along with the employee's name and their respective allocated email ID.
 
-SELECT
-    PROJECT.ID,
-    PROJECT.PROJECT_NAME,
-    EMPLOYEE.FIRST_NAME,
-    EMPLOYEE.LAST_NAME,
-    EMPLOYEE.EMAIL_ID
+SELECT PROJECT.PROJECT_ID, EMPLOYEE.EMP_ID, PROJECT.PROJECT_NAME, EMPLOYEE.FIRST_NAME, EMPLOYEE.LAST_NAME, EMPLOYEE.EMAIL_ID
 FROM EMPLOYEE
-    RIGHT JOIN PROJECT ON EMPLOYEE.ID = PROJECT.EMP_ID;
+    RIGHT JOIN PROJECT ON EMPLOYEE.EMP_ID = PROJECT.EMP_ID;
 
 SELECT *
 FROM EMPLOYEE
-    RIGHT JOIN PROJECT ON EMPLOYEE.ID = PROJECT.EMP_ID;
+    RIGHT JOIN PROJECT ON EMPLOYEE.EMP_ID = PROJECT.EMP_ID;
 
 -- FULL OUTER (JOIN) :
 
@@ -228,67 +241,68 @@ FROM EMPLOYEE
 
 -- Example of Full Outer Join.
 
-CREATE TABLE
-    Customers (
-        CustomerID INT PRIMARY KEY,
-        CustomerName VARCHAR(50)
-    );
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY,
+    CustomerName VARCHAR(250)
+);
 
 INSERT INTO
     Customers (CustomerID, CustomerName)
-VALUES (1, 'Alice'), (2, 'Bob'), (3, 'Charlie');
+VALUES (1, 'Alice'),
+    (2, 'Bob'),
+    (3, 'Charlie');
 
 SELECT * FROM Customers;
 
-CREATE TABLE
-    Orders (
-        OrderID INT PRIMARY KEY,
-        CustomerID INT,
-        OrderDate DATE
-    );
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerID INT,
+    OrderDate DATE
+);
 
 INSERT INTO
-    Orders (OrderID, CustomerID, OrderDate)
-VALUES (101, 1, '2023-09-01'), (102, 2, '2023-09-02'), (103, 1, '2023-09-03');
+    Orders (
+        OrderID,
+        CustomerID,
+        OrderDate
+    )
+VALUES (101, 1, '2023-09-01'),
+    (102, 2, '2023-09-02'),
+    (103, 1, '2023-09-03');
 
 SELECT * FROM Orders;
 
-SELECT
-    Customers.CustomerName,
-    Orders.OrderID,
-    Orders.OrderDate
-FROM Customers FULL
-    JOIN Orders ON Customers.CustomerID = Orders.CustomerID
-ORDER BY Customers.CustomerID;
+SELECT Customers.CustomerName, Customers.CustomerID, Orders.OrderID, Orders.OrderDate
+FROM Customers
+    LEFT JOIN Orders ON Customers.CustomerID = Orders.CustomerID
+UNION
+SELECT Customers.CustomerName, Customers.CustomerID, Orders.OrderID, Orders.OrderDate
+FROM Orders
+    RIGHT JOIN Customers ON Customers.CustomerID = Orders.CustomerID
+    -- CROSS JOIN
+    -- List out all the combinations possible for the employee's name and projects that can exist.
 
--- CROSS JOIN
-
--- List out all the combinations possible for the employee's name and projects that can exist.
-
-SELECT
-    EMPLOYEE.ID,
-    EMPLOYEE.FIRST_NAME,
-    EMPLOYEE.LAST_NAME,
-    PROJECT.ID,
-    PROJECT.PROJECT_NAME
+SELECT EMPLOYEE.EMP_ID, EMPLOYEE.FIRST_NAME, EMPLOYEE.LAST_NAME, PROJECT.PROJECT_ID, PROJECT.PROJECT_NAME
 FROM EMPLOYEE
     CROSS JOIN PROJECT;
 
 -- SUB QUERIES
 
--- WHERE Cluase Same Table
+-- WHERE Clause Same Table
 
 -- Employee With Age > 30
 
 SELECT *
 FROM EMPLOYEE
-WHERE AGE IN(
+WHERE
+    AGE IN (
         SELECT AGE
         FROM EMPLOYEE
-        WHERE AGE > 30
+        WHERE
+            AGE > 30
     );
 
--- WHERE Cluase Diffrent Table.
+-- WHERE Clause Different Table.
 
 -- Employee Details Working in More Than 1 Projects.
 
