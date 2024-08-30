a9af2f4745ab00f6d6fa251b0f5d36dfc9139adc
Krushi24112002 <krushimonpara24@gmail.com>
2024-08-30 13:39:11 +0530

    TRANSACTION CONTROL LANGUAGE

---
+++ TCL/TCL.sql
@@ -0,0 +1,5 @@
+-- TCL (Transaction control language): to manage transactions done in the DB
+-- 1.START TRANSACTION: begin a transaction
+-- 2.COMMIT: apply all the changes and end transaction
+-- 3.ROLLBACK: discard changes and end transactions
+-- 4.SAVEPOINT: checkout within the group of transactions in which to rollback.
